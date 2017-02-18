4d2858d18882d54dff482a38a3c353d4d57e31fb
Mike Zrimsek <mikezrimsek@gmail.com>
2017-02-17 23:33:40 -0500

    Added hiding tota11y toolbar on logo click

---
+++ src/js/content.js
@@ -0,0 +1,3 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  var tota11yToolbar = $('#tota11y-toolbar').toggle();
+});
--- src/js/popup.js
+++
@@ -1,6 +0,0 @@
-var s = document.createElement('script');
-s.src = chrome.extension.getURL('src/js/tota11y.js');
-s.onload = function() {
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(s);
