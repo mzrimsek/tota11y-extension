6a22f7000978f9fb1b67f7a21bfe4307bd016a2f
Mike Zrimsek <mikezrimsek@gmail.com>
2017-02-24 21:12:40 -0500

    fixed toggling tota11y toolbar

--- src/js/content.js
+++ src/js/content.js
@@ -5,5 +5,14 @@ window.onload = function(){
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   var tota11yToolbar = document.getElementById('tota11y-toolbar');
-  tota11yToolbar.classList.toggle('hidden');
+  var classList = tota11yToolbar.classList;
+
+  if(classList.contains("hidden")){
+    classList.remove("hidden");
+    classList.add("shown");
+  }
+  else {
+    classList.remove("shown");
+    classList.add("hidden");
+  }
 });
